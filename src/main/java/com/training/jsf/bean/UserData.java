187f7814ef48e6520e3f86c89b82d9b5fb737b0c
mkumar <test@mail.com>
2023-01-06 11:21:19 +0530

    jsf jdbc changes

--- src/main/java/com/training/jsf/bean/UserData.java
+++ src/main/java/com/training/jsf/bean/UserData.java
@@ -1,27 +1,70 @@
 package com.training.jsf.bean;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 import javax.faces.bean.ManagedBean;
-import javax.faces.bean.SessionScoped;
+import javax.faces.bean.RequestScoped;
+
 
 @ManagedBean(name="data")
-@SessionScoped
+@RequestScoped
 public class UserData {
 	
 	private String usrClass;
 	private String name;
 	private int rollNo;
 	private int age;
-	 
-	private static final List<Student> studList = new ArrayList<>(Arrays.asList(
-			new Student("Rohan","Civil", 101, 21), new Student("David","Mech", 102, 22),
-			new Student("Priya","CS", 103, 21),new Student("Ragini","E&TC", 104, 21)));
+		 
+	private static final List<Student> studList = new ArrayList<>();
+	
 	
 	public List<Student> getStudents(){
+		
+	if(studList.size() == 0) {
+		
+	
+    Connection con = null;
+		
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+		 
+	        //Scanner sc = new Scanner(System.in);
+	        String url = "jdbc:mysql://localhost:3306/infinite";
+	        String user = "root";
+	        String pass = "pass123";
+	        con = DriverManager.getConnection(url,user,pass);
+	        Statement stmt = con.createStatement();
+	        ResultSet rs = stmt.executeQuery("select * from student");
+	        while(rs.next()) {
+	        	studList.add(new Student(rs.getString(1), rs.getString(2), rs.getInt(3), rs.getInt(4)));
+	        	
+	        }
+	       
+		 }  
+	        catch (Exception e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}  
+		finally {
+			try {
+				con.close();
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+				
 		return studList;
+	}
+	else {
+		return studList;
+	}
 	}
 	
 	public void addStudent() {
